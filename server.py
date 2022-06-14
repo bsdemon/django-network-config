0987b28e1230671aa3ad5abbe8c657cb75386d4d
Ilia Iliev <eon@abv.bg>
2022-06-14 16:38:36 +0300

    Server added error response code based on delay

--- server.py
+++ server.py
@@ -3,39 +3,47 @@ import json
 import random
 import time
 
+
 class Server(BaseHTTPRequestHandler):
 
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        
+
     def do_HEAD(self):
         self._set_headers()
-        
+
     def do_GET(self):
         self.respond('GET')
-    
+
     def do_POST(self):
         self.respond('POST')
 
     def respond(self, arg0):
-        delay = random.randint(1,3)
+        delay = random.randint(10, 30)
         time.sleep(delay)
-        json_msg = json.dumps({'Status': 'Ok', 'Methond': arg0, 'Delay': delay})
+        ok_resp = {'Status': 'Ok', 'Methond': arg0, 'Delay': delay}
+        err_resp = {'Status': 'Error', 'Methond': arg0, 'Delay': delay,
+                    'Message': 'Unrecognized command: `copy running-configuration startup-configuration`'}
+        json_msg = json.dumps(ok_resp)
+        if delay > 15 and delay < 25:
+            json_msg = json.dumps(err_resp)
         self._set_headers()
         self.wfile.write(bytes(json_msg, encoding='utf8'))
-        
+
+
 def run(server_class=HTTPServer, handler_class=Server, port=8008):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    
-    print ('Starting httpd on port %d...' % port)
+
+    print('Starting httpd on port %d...' % port)
     httpd.serve_forever()
-    
+
+
 if __name__ == "__main__":
     from sys import argv
-    
+
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
