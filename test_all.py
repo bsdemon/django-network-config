f0ae5fad869c5adf30458b5be4404ba9509e1806
Ilia Iliev <eon@abv.bg>
2022-06-15 11:03:17 +0300

    Create or update 20 devices

--- test_all.py
+++ test_all.py
@@ -8,10 +8,10 @@ echo ''
 # "10.10.10.5" "10.10.10.6" "10.10.10.7" "10.10.10.8" "10.10.10.9" "10.10.10.10" "10.10.10.11" "10.10.10.12")
 # for IP in "${ip_range[@]}"
 
-for IP in 10.10.10.{2..50}
+for IP in 10.10.10.{2..20}
 do
     curl -X POST -H "Content-Type: application/json" -d \
-    '{"hostname": "sof-lab-'$IP'", "port": 8008, "ip": "'$IP'", "configuration": "configure terminal\ninterface xg0/0/1\nvlan 10 type tagged\nvlan 11 type tagged\nmac-address-learning disable\nmac-address static de:ad:00:be:ef:00:01\nsave\n" }' \
+    '{"hostname": "'$IP'", "port": 8008, "ip": "'$IP'", "configuration": "configure terminal\ninterface xg0/0/1\nvlan 10 type tagged\nvlan 11 type tagged\nmac-address-learning disable\nmac-address static de:ad:00:be:ef:00:01\nsave\n" }' \
   http://localhost:8000/api/create_task
 
   echo '\n'
